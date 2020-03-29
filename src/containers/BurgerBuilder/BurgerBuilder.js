221a36dc8d8d03b10260c0aeb1170ba51773753b
Mia5391 <mariakrasteva5391@gmail.com>
2020-03-29 20:47:49 -0300

    render Burger

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+    render(){
+        return(
+            <Aux>
+                <Burger></Burger>
+                <div>Build control</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
