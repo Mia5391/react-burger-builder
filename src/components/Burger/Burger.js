a6ee2ef7a36d787e8f133ab0aad7c26d5bcd8d83
Mia5391 <mariakrasteva5391@gmail.com>
2020-03-31 23:12:13 -0300

    add BuildControls components and project css styling

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
         return [...Array(props.ingredients[ingredientKey])].map((_, i) =>{
             return <BurgerIngredient key={ingredientKey +i} type ={ingredientKey}></BurgerIngredient>
         });
-    });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add ingredients to your burger!</p>
 
+    }
+console.log(transformedIngredients);
 
     return (
         <div className={classes.Burger}>
