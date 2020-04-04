12ecc9336433eadda69c1863c951ef5f3d792372
Mia5391 <mariakrasteva5391@gmail.com>
2020-04-04 20:56:56 -0300

    implement closing of sidebar when the backdrop is clicked (mobile)

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import classes from './SideDrawer.module.css';
+import Aux from '../../../hoc/Aux';
 
 
 const sideDrawer = (props) => {
-    // ..
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
-        <div className={classes.SideDrawer}>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={attachedClasses.join(' ')}>
         <div className={classes.Logo}> <Logo/></div>
             <nav>
                 <NavigationItems/>
             </nav>
 
         </div>
+        </Aux>
     );
-}
+};
 
 export default sideDrawer;
