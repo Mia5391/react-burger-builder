b44e841ff69e31208d81eabbe4fd66369bb34c27
Mia5391 <mariakrasteva5391@gmail.com>
2020-04-04 16:46:24 -0300

    implement toolbar nav and logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
-import classes from './Layout.module.css';
 
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Aux>
-    <div>Toolbar, SideDrawer, Backdrop </div>
-    <main className={classes.Content}>
-        {props.children}
-    </main>
+        <Toolbar />
+        <main className={classes.Content}>
+            {props.children}
+        </main>
     </Aux>
-
 );
 
 export default layout;
-
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import burgerLogo from '../../assets/images/burger-logo.png';
+
+import classes from './Logo.module.css';
+
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt="Cosi Burger" />
+    </div>
+);
+
+export default logo;
