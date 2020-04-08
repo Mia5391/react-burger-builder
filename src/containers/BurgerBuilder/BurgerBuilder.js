51e4505524a4e9a3c47e09acfc256ca13e23d5d1
Mia5391 <mariakrasteva5391@gmail.com>
2020-04-07 21:56:01 -0400

    implement loader and modal close on continue

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -24,7 +25,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchaseable: false,
-        checkoutButtonClicked: false
+        checkoutButtonClicked: false,
+        loading: false
     }
 
     updatePurchaseStateHandler(ingredients) {
@@ -78,6 +80,7 @@ class BurgerBuilder extends Component {
     }
     purchaseContinueHandler = () => {
         // alert('Yay!');
+        this.setState({loading:true});
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -92,9 +95,13 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'bike'
         }
-        axios.post("/orders.json", order) //'.json' is for Firebase only 
-        .then(response => console.log(response))
-        .catch(error => console.log(error)); 
+        axios.post("/orders.json", order) //'.json' is for Firebase only
+        .then(response => {
+            this.setState({loading:false, checkoutButtonClicked:false});
+        })
+        .catch(error => {
+            this.setState({loading:false, checkoutButtonClicked:false});
+        }); 
 
     }
 
@@ -105,15 +112,23 @@ class BurgerBuilder extends Component {
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
+
+        let orderSummary =<OrderSummary 
+        ingredients={this.state.ingredients}
+        price={this.state.totalPrice}
+        purchaseCancelled={this.purchaseCancelHandler}
+        purchaseContinued={this.purchaseContinueHandler}/>
+        if (this.state.loading) {
+            orderSummary = <Spinner/>
+
+        }
+
         return(
             <Aux>
                 <Modal show={this.state.checkoutButtonClicked}
                 modalClosed={this.purchaseCancelHandler}>
-                <OrderSummary 
-                    ingredients={this.state.ingredients}
-                    price={this.state.totalPrice}
-                    purchaseCancelled={this.purchaseCancelHandler}
-                    purchaseContinued={this.purchaseContinueHandler}/>
+                    {orderSummary}
+
                 </Modal>
                 <Burger ingredients={this.state.ingredients}></Burger>
                 <BuildControls 
