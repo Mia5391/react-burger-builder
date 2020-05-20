8c33200338ba90bbd15dc185ae637d60e212d4c6
Mia5391 <mariakrasteva5391@gmail.com>
2020-05-19 22:57:04 -0400

    implement checkout and contact form with redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -43,17 +43,7 @@ class BurgerBuilder extends Component {
         this.setState({ checkoutButtonClicked: false });
     }
     purchaseContinueHandler = () => {
-        const queryParams = [];
-        for (let i in this.state.ingredients){
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&');
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-
-        });
+        this.props.history.push('/checkout');
 
     }
 
